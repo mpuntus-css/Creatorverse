28cc01b0a19c6dbaf5515453e7face02ab584466
Nicholaus <mpuntus@css.edu>
2025-08-24 18:44:57 -0500

    Initial code

---
+++ src/pages/ReadPosts.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import Card from '../components/Card'
+import { supabase } from '../client'
+
+
+const ReadPosts = (props) => {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const {data} = await supabase
+            .from('Posts')
+            .select()
+            .order('created_at', { ascending: true })
+
+            setPosts(data)
+        }
+        fetchPosts()
+    }, [props])
+    
+    return (
+        <div className="ReadPosts">
+            {
+                posts && posts.length > 0 ?
+                [...posts]
+                .sort((a, b) => a.id - b.id)
+                .map((post,index) => 
+                    <Card 
+                        key={post.id}
+                        id={post.id} 
+                        title={post.title}
+                        author={post.author}
+                        description={post.description}
+                        betCount={post.betCount}
+                    />
+                ) : <h2>{'No Challenges Yet 😞'}</h2>
+            }
+        </div>  
+    )
+}
+
+export default ReadPosts
